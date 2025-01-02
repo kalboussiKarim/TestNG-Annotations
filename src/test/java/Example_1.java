6f69284535efd2302b3fdbc99699f884363fa302
kalboussi karim <kalboussikarim3@gmail.com>
2025-01-02 14:14:51 +0100

    understanding AfterMethod & BeforeMethod

--- src/test/java/Example_1.java
+++ src/test/java/Example_1.java
@@ -1,2 +1,26 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class Example_1 {
+
+    @BeforeMethod
+    public void login(){
+        System.out.println("Login to the application...");
+    }
+
+    @Test(priority = 1)
+    public void search(){
+        System.out.println("Searching...");
+    }
+
+    @Test(priority = 2)
+    public void advancedSearch(){
+        System.out.println("Advanced search...");
+    }
+
+    @AfterMethod
+    public void logout(){
+        System.out.println("Logout from the application...");
+    }
 }
